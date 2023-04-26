280c84bbaa7712e72979514e2afe245748153b37
Mateus Lodi <mmischel@hotmail.com>
2023-04-25 21:08:04 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  title = 'geography-game-client';
+  countries: any;
+
+  
+  constructor(private http: HttpClient){}
+
+  ngOnInit(): void {
+    this.http.get('https://5e8tjojjdl.execute-api.eu-central-1.amazonaws.com/Stage/get-flag').subscribe({
+      next: response => this.countries = response,
+      error: error => console.log(error),
+      complete:() => console.log('Request completed')
+    })
+  }
 }
